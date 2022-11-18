f5a585df2eb227e8fe61867bc1331cb79c2c9954
Metaray <metaray21@gmail.com>
2022-11-18 12:48:27 +0300

    Import project

---
+++ NoSleep/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NoSleep")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NoSleep")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("e997ccd6-4a02-466a-a91b-e6608200a695")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ NoSleep/WinApiHandle.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Runtime.InteropServices;
+
+namespace NoSleep
+{
+	public class WinApiHandle : SafeHandleZeroOrMinusOneIsInvalid
+	{
+		public WinApiHandle() : base(true) { }
+
+		protected override bool ReleaseHandle()
+		{
+			return CloseHandle(handle);
+		}
+
+		[DllImport("kernel32.dll", SetLastError = true)]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		private static extern bool CloseHandle(IntPtr hObject);
+	}
+}
